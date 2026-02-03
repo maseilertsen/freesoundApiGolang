e82c53e47832c2f9a3677e1c0173d538ea32834e
maeilert <maeilert@stud.ntnu.no>
2026-02-03 13:13:48 +0100

    feat: add songinfo struct

--- models/sound.go
+++ models/sound.go
@@ -1,1 +1,12 @@
 package models
+
+type SongInfo []struct {
+	Count    int `json:"count"`
+	Previous any `json:"previous"`
+	Next     any `json:"next"`
+	Results  []struct {
+		ID       int    `json:"id"`
+		Name     string `json:"name"`
+		Username string `json:"username"`
+	} `json:"results"`
+}
