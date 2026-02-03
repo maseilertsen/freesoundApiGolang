8f943584aade03dc5c9944ae04060b728e7bd7d9
maeilert <maeilert@stud.ntnu.no>
2026-02-03 08:40:25 +0100

    feat: basic HTTP server

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+
+func main(){
+	
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("Port has not been set, using default 8080")
+		port = "8080"
+	}
+
+	log.Println("Starting server on port "+ port + "...")
+	log.Fatal(http.ListenAndServe(":"+port,nil))
+}
