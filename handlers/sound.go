c7bbe810ade5e4083eebfc46c2b8fcb82f9a054b
maeilert <maeilert@stud.ntnu.no>
2026-02-03 11:44:19 +0100

    feat: sound handler get returning body

--- handlers/sound.go
+++ handlers/sound.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	"freesoundApiGolang/utils"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func HandleSomething(w http.ResponseWriter, r *http.Request) {
+
+	// Load API_key from .env
+	api_key := os.Getenv("FREESOUND_API_KEY")
+
+	// Get ID as a string
+	id := strings.TrimPrefix(r.URL.Path, utils.SOUND_PATH)
+
+	if id == "" {
+		http.Error(w, "Please provide an ID", http.StatusBadRequest)
+		return
+	}
+
+	getRequest := utils.API_V2 + id + "&token=" + api_key
+	log.Println("Request URL with api: " + getRequest) // TODO remove debug
+
+	// Get request towards FreesoundAPI with ID
+	res, err := http.Get(getRequest)
+	if err != nil {
+		http.Error(w, "An unexpected error orccurred.", http.StatusInternalServerError)
+		log.Println("Failed to fetch data with GET")
+	}
+	log.Println("From API: ") // todo remove
+	log.Println(res)          // todo remove
+	defer res.Body.Close()
+
+}
--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"freesoundApiGolang/handlers"
+	"freesoundApiGolang/utils"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func main() {
 
-func main(){
-	
+	err := godotenv.Load() // looks for root .env
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Println("Port has not been set, using default 8080")
 		port = "8080"
 	}
 
-	log.Println("Starting server on port "+ port + "...")
-	log.Fatal(http.ListenAndServe(":"+port,nil))
+	http.HandleFunc("GET "+utils.ROOT_PATH, handlers.RootHandler)
+	http.HandleFunc("GET "+utils.SOUND_PATH+"{id}/", handlers.HandleSomething)
+
+	log.Println("Starting server on port " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
