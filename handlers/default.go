87002dff524c47998e05e52de65436511728e99b
maeilert <maeilert@stud.ntnu.no>
2026-02-03 11:43:03 +0100

    feat: root handler + constant utils.

---
+++ handlers/default.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"fmt"
+	"freesoundApiGolang/utils"
+	"log"
+	"net/http"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+
+	// Offer information for redirection to paths
+	output := "This service does not provide any functionality on root path level. Please use paths <a href=\"" +
+		utils.ROOT_PATH + "\">"
+
+	// Write output to client
+	_, err := fmt.Fprintf(w, "%v", output)
+	log.Println("Entered root handler " + utils.ROOT_PATH)
+
+	if err != nil {
+		http.Error(w, "An unexpected error occurred. Please try again later.", http.StatusInternalServerError)
+	}
+}
---
+++ utils/consts.go
@@ -0,0 +1,8 @@
+package utils
+
+// Paths
+const ROOT_PATH = "/"
+const SOUND_PATH = "/song/"
+
+// FREESOUND API
+const API_V2 = "https://freesound.org/apiv2/search/text/?query="
