19de493b8576d9a8e33023673b578b2d6fbc02fa
maeilert <maeilert@stud.ntnu.no>
2026-02-03 12:58:01 +0100

    chore: int-check in its own function

--- handlers/sound.go
+++ handlers/sound.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,8 +17,15 @@ func HandleSongId(w http.ResponseWriter, r *http.Request) {
 	// Get ID as a string
 	id := strings.TrimPrefix(r.URL.Path, utils.SOUND_PATH)
 
+	// ID is empty
 	if id == "" {
 		http.Error(w, "Please provide an ID", http.StatusBadRequest)
+		return
+	}
+	// ID is not numeric
+	if stringIsInt(id) != 1 {
+		http.Error(w, "ID's can only be numbers.", http.StatusBadRequest)
+		log.Println("ID provided is not numeric")
 		return
 	}
 
@@ -29,10 +37,20 @@ func HandleSongId(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "An unexpected error orccurred.", http.StatusInternalServerError)
 		log.Println("Failed to fetch data with GET")
+		return
 	}
 	defer res.Body.Close()
 
 	// TODO: check that ID is only numeric
 	// TODO: Check that only one item is returned
 
+}
+
+// Returns 1 if string is int
+// Return -1 if string isnt int
+func stringIsInt(id string) int {
+	if _, err := strconv.Atoi(id); err != nil {
+		return -1
+	}
+	return 1
 }
