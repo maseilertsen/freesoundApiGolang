8677a3a0e0ffaa2cd5b0fb7cebff0fc741bce986
maeilert <maeilert@stud.ntnu.no>
2026-02-04 15:06:09 +0100

    feat: header component

---
+++ frontend/freesound-client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+   return (
+    <div >
+      <header className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800 p-2">
+          Freesound API Wrapper
+        </h1>
+        <p className="text-gray-600 mb-2">
+          This is an app using the frontend of React and Vite.
+        </p>
+      </header>
+    </div>
+   )
+}
